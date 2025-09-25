99133b9b533226b19d87fb4beb6d6ff76c0afc7c
bharathip123 <bharathip0528@gmail.com>
2025-09-25 17:56:47 -0600

    updated the file

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -5,6 +5,6 @@ Name="Bharathi"
 sum=$(($a + $b + $Name))
 echo "sum of the : ${sum}"
 Fruits=("Apple" "Bananna" "Orange")
-echo " Fruits are : $@"
+echo " Fruits are : ${Fruits[@]}"
 echo "Fruits are : ${Fruits[0]}"
 echo "Fruits: $Fruits[7]"
