43b6cb38a3f90037fdb397fa58411c8ba76d4602
bharathip123 <bharathip0528@gmail.com>
2025-09-25 16:34:58 -0600

    updated the file

--- 07-dynamiccommandvariable.sh
+++ 07-dynamiccommandvariable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DATE=$(date)
 echo "date is $DATE"
-start_Time=$(date %s)
+start_Time=$(date +%s)
 
 echo "Script started time : $start_Time"
 
 sleep 10
 
-end_time=$(date %s)
+end_time=$(date +%s)
 
 echo "Script end time: (($start_Time - $end_time))"
