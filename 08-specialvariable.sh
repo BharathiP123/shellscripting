5d8dcde640a3fc8531ff208eec4cd25d238daed0
bharathip123 <bharathip0528@gmail.com>
2025-09-25 16:54:17 -0600

    updated the files

--- 08-specialvariable.sh
+++ 08-specialvariable.sh
@@ -2,3 +2,12 @@
 
 echo "All the varables passed to the script: $@"
 echo "All the variables passed to the script: $*"
+echo "name of the script: $0"
+echo "process id the script: $$"
+echo "Who is running the script: $USER"
+echo  "current directory: $PWD"
+echo "home directory the script: $HOME"
+
+sleep 10 &
+
+echo "last command execution process id: $!"
