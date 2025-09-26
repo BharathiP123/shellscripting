b3d979c508cf7319174bf4f62f2ceeaba0249337
bharathip123 <bharathip0528@gmail.com>
2025-09-26 09:22:35 -0600

    updated the file

--- 10-if conditions.sh
+++ 10-if conditions.sh
@@ -2,6 +2,8 @@
 Num=$1
 if [ $Num -lt 20 ]; then 
     echo "Give numver is lesss than 20"
+elif [ $Num -eq 20 ]
+    echo "Given number is equal to 20: "
 else
     echo "Given numver is nt a less than $Num"
 fi
