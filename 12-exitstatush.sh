7a9d43479defe72c6257c13e459994e2d8b8c716
bharathip123 <bharathip0528@gmail.com>
2025-09-26 09:49:32 -0600

    updated the file

--- 12-exitstatush.sh
+++ 12-exitstatush.sh
@@ -2,7 +2,7 @@
 ##Checking the whether user have a root access or not . If the user has nt root access means its nnt equal to zero
 User_id=$(id -u)
 
-if [$User_id -ne 0]; then
+if [ $User_id -ne 0 ]; then
     echo "User doesnt root previliages"
 else
     echo "USer have root previliages"
